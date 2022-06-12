5da88cad7facf5bb3e15c5cd85e39fdc66f31841
harshbhagtani1 <harsh.bhagtani1@gmail.com>
2022-06-12 23:53:51 +0530

    added responsive navbar

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import hb from "./assets/HB.webp";
+import ham from "./assets/ham.png";
 
 function Header() {
+  const [resmenu, setresmenu] = useState("none");
+
   const scrolll = (e, id) => {
     e.preventDefault();
 
@@ -46,6 +49,33 @@ function Header() {
       </div>
 
       <div className="left">
+        <div
+          className="hamburg"
+          onClick={() => {
+            if (resmenu == "none") setresmenu("flex");
+            else setresmenu("none");
+          }}
+        >
+          <img src={ham}></img>
+          <div className="navmenu" style={{ display: resmenu }}>
+            <a href="#homee">Home</a>
+            <a href="#aboutt" onClick={(e) => scrolll(e, "aboutt")}>
+              About
+            </a>
+            <a href="#skillss" onClick={(e) => scrolll(e, "skillss")}>
+              Skills
+            </a>
+            <a href="#educationn" onClick={(e) => scrolll(e, "educationn")}>
+              Education
+            </a>
+            <a href="#projectss" onClick={(e) => scrolll(e, "projectss")}>
+              Projects
+            </a>
+            <a href="#experirncee" onClick={(e) => scrolll(e, "experirncee")}>
+              Experience
+            </a>
+          </div>
+        </div>
         <a href="#homee">Home</a>
         <a href="#aboutt" onClick={(e) => scrolll(e, "aboutt")}>
           About
